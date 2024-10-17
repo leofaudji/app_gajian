01fb13d9d7357ccf0dbdb6af36004c632fe7292e
leofaudji <leofaudji@gmail.com>
2024-10-17 15:56:21 +0700

    edit dashboard

--- il/bmods/v1/controllers/dash/D.php
+++ il/bmods/v1/controllers/dash/D.php
@@ -27,101 +27,28 @@ class D extends Bismillah_Controller{
         $this->set_response($re, Bismillah_Controller::HTTP_OK);
     }
 
-    public function lab_c_get(){
-        $re = array('data'=>array(), 'categories'=>array());
-        $red = array();
-        $tgl_e = date('Y-m-d');
-        $tgl_a = date('Y-m-d', strtotime($tgl_e) - (86400 * 7));
-        
-
-        $reda = array();
-        foreach($red as $tipe => $vred){
-            $reda[] = $vred;
-        }
-
-        $re['data'] = $reda;
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function lab_a_get(){
-        $re = array();
-
-        $tgl = date('Y-m-d');
-        $bom = date_bom($tgl);
-        $eom = date_eom($tgl);
-
-        $lab = array('n'=>0, 'b'=>0, 'h'=>0, 'a'=>0);
-        
-        $re['lab'] = $lab;
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function lab_a_c_get(){
-        $re = array('data'=>array(), 'categories'=>array());
-        $red = array();
-        $rec = array();
-        $tgl_e = date('Y').'-12-31';
-        $tgl_a = date('Y').'-01-01';
-        
-        $reda = array();
-        foreach($red as $vred){
-            unset($vred['datas']);
-            $reda[] = $vred;
-        }
-
-        $re['data'] = $reda;
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function kasir_w(){
-        if(!$this->lapp){
-            // $this->db->where('cabang', $this->aruser['datas']['cabang']);
-        } 
-    }
-
-    public function pj_get($periode=''){
+    public function periode_get($periode=''){   
         // sleep(15);
-        $re = array('nom'=>0,'fkt'=>0);
+        $re = array();
         if(empty($periode))$periode = 'h';
         
-        $tgl = date("Y-m-d");
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-        $tglawal = $tgl;
-        $tglakhir = $tgl;
-
-        if($periode == 'b'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-        }else if($periode == 't'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,1,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,12,31,$thn));
-        }
-
-        $re['periode'] = $this->ketperiode($periode);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pj")
+        $this->db->where('status',"1"); 
+        $db = $this->Bdb->db->select("id,kode,keterangan")
+                            ->from("gj_periode")
                             ->get();
-        $r  = $db->row_array();
-        $re['fkt'] = floatval($r['fkt']);
-        $re['nom'] = floatval($r['nom']);
+        $r  = $db->row_array();   
+        $re = array('kode'=>$r['kode'],'keterangan'=>$r['keterangan']) ;
         $this->set_response($re, Bismillah_Controller::HTTP_OK);
     }
 
 
-    public function pb_get($periode=''){
+    public function kry_get($periode=''){
         $re = array('nom'=>0,'fkt'=>0);
         if(empty($periode))$periode = 'h';
         
         $tgl = date("Y-m-d"); 
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
+        $thn = date("Y");
+        $bln = date("m");
 
         $tglawal = $tgl;
         $tglakhir = $tgl;
@@ -136,12 +63,11 @@ class D extends Bismillah_Controller{
 
         $re['periode'] = $this->ketperiode($periode);
 
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
+        //$this->db->where('tgl >=',$tglawal);   
+        $this->db->where('tgl_keluar <=',$tglakhir);
 
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pb")
+        $db = $this->Bdb->db->select("count(id) fkt,id nom")
+                            ->from("mst_karyawan") 
                             ->get();
         $r  = $db->row_array();
         $re['fkt'] = floatval($r['fkt']);
@@ -149,13 +75,13 @@ class D extends Bismillah_Controller{
         $this->set_response($re, Bismillah_Controller::HTTP_OK);
     }
 
-    public function pj_r_get($periode=''){
+    public function abs_get($periode=''){
         $re = array('nom'=>0,'fkt'=>0);
         if(empty($periode))$periode = 'h';
         
         $tgl = date("Y-m-d");
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
+        $thn = date("Y");
+        $bln = date("m");
 
         $tglawal = $tgl;
         $tglakhir = $tgl;
@@ -171,237 +97,17 @@ class D extends Bismillah_Controller{
         $re['periode'] = $this->ketperiode($periode);
 
         $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
+        $this->db->where('tgl <=',$tglakhir); 
+        $this->db->where('abs_status<>','001'); 
 
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pj_rtr")
-                            ->get();
+        //echo $tglakhir ;
+        
+        $db = $this->Bdb->db->select("count(id) fkt,id nom")
+                            ->from("abs_tmp")    
+                            ->get(); 
         $r  = $db->row_array();
         $re['fkt'] = floatval($r['fkt']);
-        $re['nom'] = floatval($r['nom']);
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function pb_r_get($periode=''){
-        $re = array('nom'=>0,'fkt'=>0);
-        if(empty($periode))$periode = 'h';
-        
-        $tgl = date("Y-m-d");
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-        $tglawal = $tgl;
-        $tglakhir = $tgl;
-
-        if($periode == 'b'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-        }else if($periode == 't'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,1,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,12,31,$thn));
-        }
-
-        $re['periode'] = $this->ketperiode($periode);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pb_trt")
-                            ->get();
-        $r  = $db->row_array();
-        $re['fkt'] = floatval($r['fkt']);
-        $re['nom'] = floatval($r['nom']);
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function pp_get($periode=''){
-        $re = array('nom'=>0,'fkt'=>0);
-        if(empty($periode))$periode = 'h';
-        
-        $tgl = date("Y-m-d");
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-        $tglawal = $tgl;
-        $tglakhir = $tgl;
-
-        if($periode == 'b'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-        }else if($periode == 't'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,1,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,12,31,$thn));
-        }
-
-        $re['periode'] = $this->ketperiode($periode);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pj_piut_pelunasan")
-                            ->get();
-        $r  = $db->row_array();
-        $re['fkt'] = floatval($r['fkt']);
-        $re['nom'] = floatval($r['nom']);
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function ph_get($periode=''){
-        $re = array('nom'=>0,'fkt'=>0);
-        if(empty($periode))$periode = 'h';
-        
-        $tgl = date("Y-m-d");
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-        $tglawal = $tgl;
-        $tglakhir = $tgl;
-
-        if($periode == 'b'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-        }else if($periode == 't'){
-            $tglawal = date("Y-m-d",mktime(0,0,0,1,1,$thn));
-            $tglakhir = date("Y-m-d",mktime(0,0,0,12,31,$thn));
-        }
-
-        $re['periode'] = $this->ketperiode($periode);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-
-        $db = $this->Bdb->db->select("count(id) fkt,ifnull(sum(total),0) nom")
-                            ->from("pb_htg_pelunasan")
-                            ->get();
-        $r  = $db->row_array();
-        $re['fkt'] = floatval($r['fkt']);
-        $re['nom'] = floatval($r['nom']);
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function plgn_get(){
-        $re = array();
-        
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-
-        $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-        $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-        $this->db->where('status',"1");
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("pelanggan")
-                            ->get();
-        $r  = $db->row_array();
-        $re['jml'] = floatval($r['jml']);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-        $this->db->where('kd_pelanggan is not null');
-        $db = $this->Bdb->db->select("id")
-                            ->from("pj")
-                            ->group_by("kd_pelanggan")
-                            ->get();
-        $pj  = $db->num_rows();
-
-        $re['pj'] = floatval($pj);
-
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function so_get(){
-        $re = array();
-        
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-
-        $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-        $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("pj_so")
-                            ->get();
-        $r  = $db->row_array();
-        $re['jml'] = floatval($r['jml']);
-
-        $this->db->where('t.tgl >=',$tglawal);
-        $this->db->where('t.tgl <=',$tglakhir);
-        $this->db->where('t.status',"1");
-        $db = $this->Bdb->db->select("b.id")
-                            ->from("pj_so_barang b")
-                            ->join("pj_so t","t.faktur = b.faktur")
-                            ->group_by("b.kd_barang")
-                            ->get();
-        $sob  = $db->num_rows();
-
-        $re['brg'] = floatval($sob);
-
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function pjkrm_get(){
-        $re = array();
-        
-        $thn = 2022;//date("Y");
-        $bln = 4;//date("m");
-
-
-        $tglawal = date("Y-m-d",mktime(0,0,0,$bln,1,$thn));
-        $tglakhir = date("Y-m-d",mktime(0,0,0,$bln+1,0,$thn));
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("pj_kirim")
-                            ->get();
-        $r  = $db->row_array();
-        $re['krm'] = floatval($r['jml']);
-
-        $this->db->where('tgl >=',$tglawal);
-        $this->db->where('tgl <=',$tglakhir);
-        $this->db->where('status',"1");
-        $this->db->where('status_faktur',"2");
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("pj_kirim")
-                            ->get();
-        $r  = $db->row_array();
-
-        $re['tkrm'] = floatval($r['jml']);
-
-        $this->set_response($re, Bismillah_Controller::HTTP_OK);
-    }
-
-    public function brg_get(){
-        $re = array();
-        
-        $this->db->where('status',"1");
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("stk_barang")
-                            ->get();
-        $r  = $db->row_array();
-        $re['brg'] = floatval($r['jml']);
-
-        $this->db->where('status',"1");
-        $this->db->where('JSON_VALUE(saldo,"$.saldo")',0);
-        $db = $this->Bdb->db->select("count(id) jml")
-                            ->from("stk_barang")
-                            ->get();
-        $r  = $db->row_array();
-
-        $re['brg_0'] = floatval($r['jml']);
-
+        $re['nom'] = floatval($r['nom']); 
         $this->set_response($re, Bismillah_Controller::HTTP_OK);
     }
 
